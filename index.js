09b31171ecd5ca7e1b15661c80fa9ba583997c3e
proactive-callee-1012 <shaemc22@gmail.com>
2020-12-26 22:51:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,6 +23,8 @@ function johnLennonFacts(facts) {
 function iLoveTheBeatles(num){
   var array = [];
   do{
-    []
-  }
+    array.push('I love the Beatles!');
+    num++;
+  } while (num < 15);
+  return beatles;
 }
