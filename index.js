4e046ae0283dc07dbe0170d0862b8fc30a42c769
proactive-callee-1012 <shaemc22@gmail.com>
2020-12-26 22:31:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function johnLennonFacts() {
   var array = [];
   var i = 0;
   while (i < facts.length) {
-    array.push(`${facts[i]} + !!!`);
+    array.push(`${facts[i]}!!!`);
     i++;
   }
   return array;
