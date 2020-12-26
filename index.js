35f7a7acf6e66a38c45bc9e9f46d0362a512ff05
proactive-callee-1012 <shaemc22@gmail.com>
2020-12-26 22:34:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function theBeatlesPlay(musicians, instruments){
 function johnLennonFacts(facts) {
   var array = [];
   var i = 0;
-  while (i < facts.length) {
+  while (i <array.length) {
     array.push(`${facts[i]}!!!`);
     i++;
   }
