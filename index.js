8964dc5e7ca70f8bf29ebce817029a38476f8f07
proactive-callee-1012 <shaemc22@gmail.com>
2020-12-26 22:19:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments){
+  let array = [];
+
+  for(let i = 0; i < musicians.length; i++){
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+
+  return array;
+}
+
+function johnLennonFacts() {
+  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+  var array = [];
+  var i = 0;
+  while (array.length < facts.length) {
+    array.push(`${facts[i]}!!!`);
+    i++;
+  }
+  return array;
+}
+
